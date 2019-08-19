dd30f2ff06718022ea784f67fc58d518c2a21d2b
mansurbekmurodov@yahoo.com <mansurbekmurodov@yahoo.com>
2019-08-18 22:17:34 -0500

    first comimt

---
+++ src/test/java/com/cybertek/tests/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.cybertek.tests;
+
+import com.github.javafaker.Faker;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        String name = "Hello world";
+        System.out.println("name = " + name);
+
+
+    }
+}
