d9497dc9efd0fb10b243b4cb465cfe174dac7a5c
mansurbekmurodov@yahoo.com <mansurbekmurodov@yahoo.com>
2019-08-19 22:00:53 -0500

    created new class

---
+++ src/test/java/com/cybertek/tests/NewClassMergebracnh.java
@@ -0,0 +1,8 @@
+package com.cybertek.tests;
+
+public class NewClassMergebracnh {
+    public static void main(String[] args) {
+        System.out.println("Hello world 2");
+        System.out.println("I shoould merge it");
+    }
+}
