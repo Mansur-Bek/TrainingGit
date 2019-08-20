85de2aab76cfe5eb19f042e8eba5bd3de4cab46e
mansurbekmurodov@yahoo.com <mansurbekmurodov@yahoo.com>
2019-08-20 01:08:26 -0500

    Bring back

--- src/test/java/com/beforestash/BeforeStashing.java
+++ src/test/java/com/beforestash/BeforeStashing.java
@@ -4,5 +4,6 @@ public class BeforeStashing {
 
     public static void main(String[] args) {
         System.out.println("It is in the merge1 branch crated before stash command");
+        System.out.println("stash 3 ");
     }
 }
