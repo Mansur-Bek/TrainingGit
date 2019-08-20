2f8859dddcd934c114bd2c878e3485bc07a69641
mansurbekmurodov@yahoo.com <mansurbekmurodov@yahoo.com>
2019-08-20 01:11:10 -0500

    merge conflict solved

--- src/test/java/com/beforestash/BeforeStashing.java
+++ src/test/java/com/beforestash/BeforeStashing.java
@@ -5,5 +5,6 @@ public class BeforeStashing {
     public static void main(String[] args) {
         System.out.println("It is in the merge1 branch crated before stash command");
         System.out.println("stash 3 ");
+        System.out.println("Stash number two");
     }
 }
