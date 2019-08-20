d2960cbc58b5fd1424e5ef317573edc8e1d1a0df
mansurbekmurodov@yahoo.com <mansurbekmurodov@yahoo.com>
2019-08-19 21:50:41 -0500

    mergetest dot feature file created

--- src/test/java/com/cybertek/tests/HelloWorld.java
+++ src/test/java/com/cybertek/tests/HelloWorld.java
@@ -14,7 +14,6 @@ public class HelloWorld {
         System.out.println("On work branch created");
         System.out.println("This change made on the remote repo on git hub");
         System.out.println("This change made on the remote repo on git hub to ceck how git fetch command works");
-// this comment was added on github remote repo
 
     }
 }
