82d563c81d5aa8d86e995326eff8395282a7bddc
mansurbekmurodov@yahoo.com <mansurbekmurodov@yahoo.com>
2019-08-20 01:39:27 -0500

    I made changes and I want push it

--- src/test/java/com/cybertek/tests/NewClassMergebracnh.java
+++ src/test/java/com/cybertek/tests/NewClassMergebracnh.java
@@ -4,5 +4,7 @@ public class NewClassMergebracnh {
     public static void main(String[] args) {
         System.out.println("Hello world 2");
         System.out.println("I shoould merge it");
+        System.out.println("My remote origin master is ahead of local master branch and i want to push this new change");
+        System.out.println("My remote origin master is ahead of local master branch and i want to push this new change");
     }
 }
